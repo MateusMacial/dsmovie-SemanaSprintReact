2203d68ef92bbb33b1464972f59a3c19292eac6b
Mateus Macial <mateus_macial@hotmail.com>
2022-01-11 16:03:49 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/MateusMacial" target='_blank'>
+                    <a href="https://github.com/MateusMacial" target='_blank' rel="noreferrer">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
                             <p className='dsmovie-contact-link'>/MateusMacial</p>
